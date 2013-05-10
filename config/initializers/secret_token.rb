ef28cf70314612534c9178586861225472b17646
荻野 浩史 <cheese.cake0611@gmail.com>
2013-05-11 00:14:49 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlineboard::Application.config.secret_token = 'be42619fe31ab732fe3e67f63483d61c17232dfd8c3db341176892fbd42efb839cf9e1ede5e84f5c3f994c327d4302d7ffe7a91f29be0ee3d569cee2d66c2aeb'
